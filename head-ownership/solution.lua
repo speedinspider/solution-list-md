85bc3d41886f11d8842c50e86f7ec06695adfb8f
speedin <thecrazynintendomadman@gmail.com>
2025-11-15 09:44:15 -0500

    Add comments for player validation logic

--- head-ownership/solution.lua
+++ head-ownership/solution.lua
@@ -4,9 +4,9 @@ local Player = game.Players:GetPlayerFromCharacter(Character)
 local headMovement = Character.HeadMovement
 
 headMovement.OnServerEvent:Connect(function(_player, cFrame)
-      if Player.UserId == _player.UserId then
+      if Player.UserId == _player.UserId then -- Check to make sure its the real player.
         -- Do head movement logic here, or whatever.
-    elseif Player.UserId ~= _player.UserId then
+    elseif Player.UserId ~= _player.UserId then -- If its not, we kick them.
          player:Kick("Not the player")
     end
   end)
